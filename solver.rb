d00ea3abbdb6019db64ab7ec47ec91145898342a
LOctavio <luis_ram1997@hotmail.com>
2021-10-29 19:58:17 -0500

    Add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -9,4 +9,11 @@ class Solver
     word.each_char { |c| new_word = c + new_word }
     return new_word
   end
+
+  def fizzbuzz(n)
+    return 'fizzbuzz' if n%3 == 0 && n%5 == 0
+    return 'fizz' if n%3 == 0
+    return 'buzz' if n%5 == 0
+    return n
+  end
 end
