a4ccbc74d4118e9b1bbbe93a8e591820f6464772
LOctavio <luis_ram1997@hotmail.com>
2021-10-29 20:01:45 -0500

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,19 +1,21 @@
 class Solver
-  def factorial(n)
+  def factorial(num)
     return 1 if n.zero?
-    return n * factorial(n-1)
+
+    num * factorial(n - 1)
   end
 
   def reverse(word)
     new_word = ''
     word.each_char { |c| new_word = c + new_word }
-    return new_word
+    new_word
   end
 
-  def fizzbuzz(n)
-    return 'fizzbuzz' if n%3 == 0 && n%5 == 0
-    return 'fizz' if n%3 == 0
-    return 'buzz' if n%5 == 0
-    return n
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (n % 3).zero? && (n % 5).zero?
+    return 'fizz' if (n % 3).zero?
+    return 'buzz' if (n % 5).zero?
+
+    num
   end
 end
