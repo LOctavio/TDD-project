c2606021df0461b07cadd684d7dd0398d684b370
LOctavio <luis_ram1997@hotmail.com>
2021-10-29 19:53:40 -0500

    Add reverse method

--- solver.rb
+++ solver.rb
@@ -3,4 +3,10 @@ class Solver
     return 1 if n.zero?
     return n * factorial(n-1)
   end
+
+  def reverse(word)
+    new_word = ''
+    word.each_char { |c| new_word = c + new_word }
+    return new_word
+  end
 end
